de3cc6531c6a314118b886a8ea042b18abd09159
Natty Davis <natty.davis79@gmail.com>
2016-12-08 11:48:38 -0500

    initial set up

---
+++ beastMode/src/index.js
@@ -0,0 +1,6 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+import App      from './components/App.jsx';
+import Landing  from './components/Landing/Landing.jsx';
+// mount our App at #container
+ReactDOM.render(<Landing />, document.querySelector('#root-container'));
